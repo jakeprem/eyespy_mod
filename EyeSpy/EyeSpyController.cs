290870c975415ca18ec8c34decd28f18583f2259
jakeprem <jakeprem@gmail.com>
2017-01-23 20:55:53 -0500

    development

--- EyeSpy/EyeSpyController.cs
+++ EyeSpy/EyeSpyController.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -16,6 +15,7 @@ namespace EyeSpy
     {
         private UdpClient client;
         private IPEndPoint remoteEndPoint;
+        private Dictionary<String, String> _colonistHashes;
 
         static EyeSpyController()
         {
@@ -28,11 +28,13 @@ namespace EyeSpy
         {
             enabled = false;
             this.client = new UdpClient();
+
+            this._colonistHashes = new Dictionary<String, String>();
+
             remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8083);
 
             Log.Message("Sending to 127.0.0.1");
-            byte[] data = Encoding.UTF8.GetBytes("Test text");
-            client.Send(data, data.Length, remoteEndPoint);
+            SendPacket("InitialPacket");            
         }
 
         public void OnLevelWasLoaded(int level)
@@ -53,31 +55,76 @@ namespace EyeSpy
             {
                 try
                 {
+                    var colonistsToSend = new List<ColonistData>();
+
                     foreach (Pawn current in PawnsFinder.AllMapsAndWorld_Alive)
                     {
                         if (current.IsColonist)
                         {
-                            var name = current.Name;
-                            var name_short = name.ToStringShort;   
-                            
-                            var text = "{\"name\": \"" + name_short + "\", \"job\": \"" + current.jobs.curDriver.GetReport().CapitalizeFirst() + "\", \"id\": \"" + current.ThingID + "\"}";
-                            byte[] data = Encoding.UTF8.GetBytes(text);
-                            client.Send(data, data.Length, remoteEndPoint);
+                            var newColonist = new ColonistData{ ID = current.ThingID, Name = current.Name.ToStringShort, CurrentJob = current.jobs.curDriver.GetReport().CapitalizeFirst() };
+                            var newColonistHash = HashColonist(newColonist);
 
+                            if (_colonistHashes.ContainsKey(newColonist.ID))
+                            {                                
+                                var currentColonistHash = _colonistHashes[newColonist.ID];                                
+                                if (currentColonistHash != newColonistHash)
+                                {
+                                    SendPacket(newColonistHash + " : " + newColonist.Name + " : " + newColonist.CurrentJob);
+
+                                    colonistsToSend.Add(newColonist);
+                                    _colonistHashes[newColonist.ID] = newColonistHash;                                                                                                            
+                                }
+
+
+                            } else
+                            {
+                                SendPacket("New colonist: " + newColonistHash + " : " + newColonist.Name + " : " + newColonist.CurrentJob);
+
+                                _colonistHashes[newColonist.ID] = newColonistHash;
+                                colonistsToSend.Add(newColonist);
+                            }
                         }
-                    }
+                    }                    
+                    
+                    //string json = JsonConvert.SerializeObject(colonistsToSend, Formatting.Indented);                    
+                    //string json = JsonHelper.ToJson(colonistsToSend.ToArray());                    
 
                 }
                 catch (Exception ex)
                 {
-                    // enabled = false;
-                    var st = new StackTrace(ex, true);
-                    var frame = st.GetFrame(0);
-                    var line = frame.GetFileLineNumber();
-                    Log.Error(line.ToString());
+                    enabled = false;
                     Log.Error(ex.Message);
                 }
             }
+        }
+
+        private void SendPacket(string textToSend)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(textToSend);
+            client.Send(data, data.Length, remoteEndPoint);
+        }
+
+        // Works but not serializing currently
+        [Serializable]
+        private class ColonistData
+        {
+            public string ID { get; set; }
+            public string Name { get; set; }
+            public string CurrentJob { get; set; }
+        }
+
+        // Works
+        private string HashColonist(ColonistData colonist)
+        {
+            var text = colonist.ID + colonist.Name + colonist.CurrentJob;
+            return Base64Encode(text);
+        }
+
+        // Works
+        private string Base64Encode(string plainText)
+        {
+            var textBytes = Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(textBytes);
         }
     }
 }
